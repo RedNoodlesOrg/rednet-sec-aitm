5c9275ed19253908807f59b696d230329100c966
FakeApate <imboden.samuel@protonmail.ch>
2024-05-28 11:39:13 +0200

    refactor: Update `aitm.msft_aama` package structure and state machine implementation
    This commit refactors the `aitm.msft_aama` package structure and updates the state machine implementation. The changes include:
    - Renaming the `state_machine.py` module to `__init__.py` and moving it to the `state_machine` subdirectory.
    - Updating import statements in `__init__.py` to reflect the new package structure.
    - Creating a new `actions.py` module in the `state_machine` subdirectory to house the state machine actions.
    - Moving relevant code from `__init__.py` to `actions.py` and updating import statements accordingly.
    - Renaming the `RegistrationStateMachine` class to `StateMachine` in `__init__.py`.
    - Updating references to `RegistrationStateMachine` in the codebase to `StateMachine`.
    
    These changes improve the organization and maintainability of the `aitm.msft_aama` package and enhance the clarity and separation of concerns in the state machine implementation.

--- tests/test_state_machine.py
+++ tests/test_state_machine.py
@@ -12,11 +12,17 @@ def state_machine():
     return RegistrationStateMachine()
 
 
-@patch("aitm.msft_aama.state_machine.add_security_info", return_value={"VerificationContext": "verification_context"})
-@patch("aitm.msft_aama.state_machine.authorize_mobileapp", return_value="session_ctx")
-@patch("aitm.msft_aama.state_machine.initialize_mobileapp_registration", return_value={"SecretKey": "5jm2hkk5jfn7s6dj"})
-@patch("aitm.msft_aama.state_machine.prepare_session", return_value="session")
-@patch("aitm.msft_aama.state_machine.verify_security_info", return_value="")
+@patch(
+    "aitm.msft_aama.state_machine.simple_state_machine.add_security_info",
+    return_value={"VerificationContext": "verification_context"},
+)
+@patch("aitm.msft_aama.state_machine.simple_state_machine.authorize_mobileapp", return_value="session_ctx")
+@patch(
+    "aitm.msft_aama.state_machine.simple_state_machine.initialize_mobileapp_registration",
+    return_value={"SecretKey": "5jm2hkk5jfn7s6dj"},
+)
+@patch("aitm.msft_aama.state_machine.simple_state_machine.prepare_session", return_value="session")
+@patch("aitm.msft_aama.state_machine.simple_state_machine.verify_security_info", return_value="")
 def test(
     mock_verify_security_info: MagicMock,
     mock_prepare_session: MagicMock,
@@ -25,10 +31,10 @@ def test(
     mock_add_security_info: MagicMock,
     state_machine: RegistrationStateMachine,
 ):
-    assert state_machine.is_IDLE
+    assert state_machine.state == state_machine.States.IDLE
 
-    state_machine.session_captured("cookies", "user_agent")
-    assert state_machine.is_IDLE
+    state_machine.start("cookies", "user_agent")
+    assert state_machine.state == state_machine.States.IDLE
     mock_verify_security_info.assert_called_once
     mock_prepare_session.assert_called_once
     mock_initialize_mobileapp_registration.assert_called_once
