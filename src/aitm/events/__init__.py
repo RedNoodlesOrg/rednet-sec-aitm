e75fddbbad0110080b21bfdc98cb29cd778e1285
Fakeapate <imboden.samuel@protonmail.ch>
2024-05-28 17:05:06 +0200

    refactor: Add MfaMethodRegisteredEvent and EventListener classes
    This commit adds the MfaMethodRegisteredEvent class, which represents an event when an MFA method is registered. It also adds the EventListener class to handle events in the listener.py module. These changes improve the functionality and organization of the codebase.

--- src/aitm/events/__init__.py
+++ src/aitm/events/__init__.py
@@ -3,4 +3,6 @@ from __future__ import annotations
 
 from .credentials_captured_event import CredentialsCapturedEvent
 from .emitter import EventEmitter
+from .listener import EventListener
+from .mfa_method_registered_event import MfaMethodRegisteredEvent
 from .mfa_session_captured_event import MfaSessionCapturedEvent
