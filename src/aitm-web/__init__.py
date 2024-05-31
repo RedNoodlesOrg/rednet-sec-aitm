c2bb019b745dee7f79eaf25b91f09d7c94d18390
Fakeapate <imboden.samuel@protonmail.ch>
2024-05-31 15:08:53 +0200

    move template into correct structre

---
+++ src/aitm-web/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from flask import Flask
+
+from .routes import main
+
+
+def create_app():
+    app = Flask(__name__)
+    # app.config.from_object("config")
+
+    app.register_blueprint(main)
+
+    return app
---
+++ src/aitm-web/routes.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from flask import Blueprint, render_template
+
+main = Blueprint("main", __name__)
+
+
+@main.route("/")
+def index():
+    return render_template("base.html")
